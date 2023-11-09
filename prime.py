614a713639e2d9f5051db02e653956eecf548ca1
Ashutosh <ashuxtosh@gmail.com>
2023-11-10 05:10:12 +0530

    prime

---
+++ prime.py
@@ -0,0 +1,12 @@
+def prime_number(n):
+    prime = 0
+    count = 0
+    num = 2
+    while count<n:
+        if all(num % item !=0 for item in range(2,num)):
+            prime += num
+            count += 1
+        num += 1
+        
+    return prime
+print(prime_number(10))
