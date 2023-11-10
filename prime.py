81bc948186a3d30b94962b533adfb0e76f3585f3
Ashutosh <ashuxtosh@gmail.com>
2023-11-11 03:34:25 +0530

    updated prime

--- prime.py
+++ prime.py
@@ -10,3 +10,17 @@ def prime_number(n):
         
     return prime
 print(prime_number(10))
+
+# Path: prime.py
+
+def list_prime(n):
+    prime = []
+    num = 2
+    while len(prime)<n:
+        if all(num % item !=0 for item in range(2,num)):
+            prime.append(num)
+        num += 1
+
+    return prime
+
+print(list_prime(10))
